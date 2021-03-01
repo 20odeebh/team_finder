68256a0cecb92fea175435e81775e8a89a346730
20odeebh <20odeebh@gmail.com>
2021-03-01 18:57:13 +0900

    Init Commit

---
+++ src/test/java/com/whereismyteam/teamfinder/TeamfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whereismyteam.teamfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
